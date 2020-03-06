6b9674a53baef120cec2a880201ffabe49b8eac8
Enmanuel <edelanuezlang@gmail.com>
2020-03-05 23:47:05 -0500

    add and save task model

--- app.js
+++ app.js
@@ -8,7 +8,27 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${p
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {
-  console.log("We're connected!")
+  console.log('Connected to mongoDB cluster!')
+
+  // Create schemas, compile to model, instantiate objects, save to db
+  const Schema = mongoose.Schema
+  const taskSchema = new Schema({
+    task: String,
+    dueDate: Date,
+    isCompleted: Boolean
+  }) // add keys later with Schema.add
+
+  const Task = mongoose.model('Task', taskSchema) // instances of models are documents
+  const goWalk = new Task({
+    task: 'Go for a walk!',
+    dueDate: Date.now(),
+    isCompleted: false
+  })
+
+  goWalk.save((error, goWalk) => {
+    if (error) return console.log(error)
+    return console.log(`Saved task ${goWalk.task}`)
+  })
 })
 
 app.get('/', (req, res) => {
