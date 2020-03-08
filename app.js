10b7616828cc422ea0ecb74ded5a987833e58734
Enmanuel <edelanuezlang@gmail.com>
2020-03-08 18:06:01 -0400

    add singup fns and route

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 require('dotenv').config()
 const bodyParser = require('body-parser')
+const user = require('./routes/user')
+
+// issue: uri malformed when moved to different file
+const mongoose = require('mongoose')
+
+const initMongoDB = async () => {
+  try {
+    await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}`, { useNewUrlParser: true, useUnifiedTopology: true })
+    console.log('Connected to mongoDB cluster!')
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
+}
+
+initMongoDB().catch(err => console.error(err))
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -7,9 +24,13 @@ const port = 3000
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
-app.get('/portfolio', (req, res) => {
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
   res.render('portfolio')
 })
+
+app.use('/user', user)
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}...`)
---
+++ routes/user.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const {check, validationResult} = require('express-validator')
+const bcrypt = require('bcryptjs')
+const jsonWebToken = require('jsonwebtoken')
+const router = express.Router();
+
+const User = require('../models/user')
+
+router.post('/sign-up', [
+	check("email").isEmail(),
+	check("password").isLength({min:6})
+	], async (req, res) => {
+		const validationErrors = validationResult(req)
+		if (!validationErrors.isEmpty()) {
+			return res.status(400).json({errors: validationErrors.array()})
+		}
+
+		const {
+			email,
+			password,
+			firstName,
+		} = req.body
+
+		try {
+			let user = await User.findOne({email})
+			if (user) {return res.status(400).json({msg: "User already exists!"})}
+			user = new User({
+				email,
+				password,
+				firstName
+			})
+
+			const salt = await bcrypt.genSalt(10)
+			user.password = await bcrypt.hash(password, salt)
+
+			await user.save()
+
+			const payload = {
+				user: {
+					id: user.id
+				}
+			}
+
+			jsonWebToken.sign(payload, 'randomString', {expiresIn: 10000}, (err, token) => {
+				if (err) throw err
+				res.status(200).json({token})
+			})
+		} catch (err) {
+			console.error(err)
+			res.status(500).send('Error saving user data')
+		}
+	})
+
+module.exports = router
