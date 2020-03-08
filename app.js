a5d81195d45e19bedc8f79b74c56c55a5d36d9b7
Enmanuel <edelanuezlang@gmail.com>
2020-03-08 16:22:56 -0400

    seperate db fns, add ejs and sec utils

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
 require('dotenv').config()
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const port = 3000
 
-const mongoose = require('mongoose')
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}`, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', () => {
-  console.log('Connected to mongoDB cluster!')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 
-  /*
-   * Create schemas, compile to model, instantiate objects, save to db
-   * First save to db!!
-    const taskSchema = new Schema({
-      task: String,
-      dueDate: Date,
-      isCompleted: Boolean
-    }) // add keys later with Schema.add
-
-    const Task = mongoose.model('Task', taskSchema) // instances of models are documents
-    const goWalk = new Task({
-      task: 'Go for a walk!',
-      dueDate: Date.now(),
-      isCompleted: false
-    })
-
-    goWalk.save((error, goWalk) => {
-      if (error) return console.log(error)
-      return console.log(`Saved task ${goWalk.task}`)
-    })
-  */
-})
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/portfolio', (req, res) => {
+  res.render('portfolio')
 })
 
 app.listen(port, () => {
