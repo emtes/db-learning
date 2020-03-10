8804719c5546f3bd8dd6d90af177a77bb0bf13ae
Enmanuel <edelanuezlang@gmail.com>
2020-03-10 09:57:38 -0400

    formatting

--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose')
 
 const initMongoDB = async () => {
-	try {
-		await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}`,
-  { useNewUrlParser: true, useUnifiedTopology: true })
-		console.log('Connected to mongoDB cluster!')
-	} catch (err) {
-		console.error(err)
-		throw err
-	}
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}`,
+      { useNewUrlParser: true, useUnifiedTopology: true }
+    )
+    console.log('Connected to mongoDB cluster!')
+  } catch (err) {
+    console.error(err)
+    throw err
+  }
 }
-
