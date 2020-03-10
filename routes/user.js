30bf361be9867435e1c481dba048a49a2c88fa04
Enmanuel <edelanuezlang@gmail.com>
2020-03-10 09:55:42 -0400

    add token verification to fetch user

--- routes/user.js
+++ routes/user.js
@@ -100,4 +100,15 @@ router.post(
   }
 )
 
+const auth = require('../middleware/auth')
+
+router.get('/me', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id)
+    res.json(user)
+  } catch (err) {
+    res.send({ message: 'Error fetching user.' })
+  }
+})
+
 module.exports = router
