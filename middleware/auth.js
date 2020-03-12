b6101bdbd84d68a59985724f54ca6a27768b73ea
Enmanuel <edelanuezlang@gmail.com>
2020-03-12 16:28:20 -0400

    formatting, cleanup

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,18 @@
-const jsonWebToken = require('jsonwebtoken')
+const jsonWebToken = require('jsonwebtoken');
 
-function auth (req, res, next) {
-  const token = req.header('token')
+function auth(req, res, next) {
+  const token = req.header('token');
 
-  if (!token) return res.status(401).json({ message: 'Auth Error' })
+  if (!token) return res.status(401).json({ message: 'Auth Error' });
 
   try {
-    const decoded = jsonWebToken.verify(token, 'secret')
-    req.user = decoded.user
-    next()
+    const decoded = jsonWebToken.verify(token, 'secret');
+    req.user = decoded.user;
+    next();
   } catch (err) {
-    console.error(err)
-    res.status(500).send({ message: 'Invalid token' })
+    console.error(err);
+    res.status(500).send({ message: 'Invalid token' });
   }
 }
 
-module.exports = auth
+module.exports = auth;
