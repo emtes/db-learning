08ca2e43dd2c9986372ee9630dc444f87e7cbfd5
Enmanuel <edelanuezlang@gmail.com>
2020-03-12 17:15:41 -0400

    add route for owned stocks data

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router()
 const auth = require('../middleware/auth');
 const Transactions = require('../models/Transaction');
+const Stocks = require('../models/Stock')
 
 router.get('/', (req, res) => {
 	res.render('transactionHistory')
@@ -9,12 +10,20 @@ router.get('/', (req, res) => {
 
 router.post('/', auth, async (req, res) => {
 	try {
-		console.log(req.id)
 		const transactions = await Transactions.find({user_id: req.user.id});
 		res.send(transactions) 
 	} catch (err) {
-		console.error('Error fetching transactions',err)
+		console.error('Error fetching transactions.',err)
 	}
 });
+
+router.post('/stocks', auth, async (req, res) => {
+	try {
+		const stocks = await Stocks.find({user_id: req.user.id})
+		res.send(stocks)
+	} catch (err) {
+		console.error('Error fetching stock data.', err)
+	}
+})
 
 module.exports = router;
