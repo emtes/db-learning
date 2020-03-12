e7ecda70fc8f1166b837f64372d2a753ad15ae75
Enmanuel <edelanuezlang@gmail.com>
2020-03-12 16:29:21 -0400

    update

--- app.js
+++ app.js
@@ -1,40 +1,50 @@
-require('dotenv').config()
-const bodyParser = require('body-parser')
-const user = require('./routes/user')
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const user = require('./routes/user');
+const buy = require('./routes/buy');
+const transactions = require('./routes/transactions');
+const app = express();
+const port = 3000;
 
 // issue: uri malformed when moved to different file
-const mongoose = require('mongoose')
 
 const initMongoDB = async () => {
   try {
     await mongoose.connect(
       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}${process.env.DB_HOST}`,
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
-    console.log('Connected to mongoDB cluster!')
+      { useNewUrlParser: true, useUnifiedTopology: true },
+    );
+    console.log('Connected to mongoDB cluster!');
   } catch (err) {
-    console.error(err)
-    throw err
+    console.error(err);
+    throw err;
   }
-}
+};
 
-initMongoDB().catch(err => console.error(err))
+initMongoDB().catch((err) => console.error(err));
 
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-
-app.use(bodyParser.json())
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.render('portfolio')
-})
+  res.render('portfolio');
+});
 
-app.use('/user', user)
+app.use('/user', user); 
+app.use('/buy', buy);
+app.use('/transactions', transactions)
+
+app.get('/log-in', (req, res) => {
+  res.render('login');
+});
+
+app.get('/sign-up', (req, res) => {
+  res.render('signup');
+});
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}...`)
-})
+  console.log(`App listening on port ${port}...`);
+});
