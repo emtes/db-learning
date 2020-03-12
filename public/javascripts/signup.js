f91a9ac8523ceaf79093bef3c4573d0c5b781756
Enmanuel <edelanuezlang@gmail.com>
2020-03-12 16:29:45 -0400

    add page logic

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -1,21 +1,21 @@
-const form = document.querySelector('form')
+const form = document.querySelector('form');
 
-form.addEventListener('submit', e => {
-  e.preventDefault()
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
   const signupDat = {
     name: form.name.value,
     email: form.email.value,
     password: form.password.value,
-    address: form.address.value
-  }
-  console.log(JSON.stringify(signupDat))
+    address: form.address.value,
+  };
+  console.log(JSON.stringify(signupDat));
   fetch('/user/sign-up', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
-    body: JSON.stringify(signupDat)
+    body: JSON.stringify(signupDat),
   })
-    .then(res => res.json())
-    .then()
-})
+    .then((res) => res.json())
+    .then();
+});
