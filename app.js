0727a113070bb887af343c34dbe1a10efb1ee28e
Enmanuel <edelanuezlang@gmail.com>
2020-03-12 19:54:26 -0400

    add dev script, import env vars

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+if (NODE_ENV === 'development') require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -7,7 +7,7 @@ const buy = require('./routes/buy');
 const transactions = require('./routes/transactions');
 const auth = require('./middleware/auth')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // issue: uri malformed when moved to different file
 
