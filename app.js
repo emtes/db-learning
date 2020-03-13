6955c452b6cd90a46cf2b6ec9b2dc748238572b1
Enmanuel dlN <edelanuezlang@gmail.com>
2020-03-13 19:42:08 -0400

    move base routers to routes/main

--- app.js
+++ app.js
@@ -1,54 +1,24 @@
-// if (NODE_ENV === 'development') require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const initMongoDB = require('./src/mongodb');
 const user = require('./routes/user');
 const buy = require('./routes/buy');
 const transactions = require('./routes/transactions');
-const auth = require('./middleware/auth')
+const main = require('./routes/main');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-// issue: uri malformed when moved to different file
-
-const initMongoDB = async () => {
-  try {
-    await mongoose.connect(
-      `mongodb+srv://enmanuel:cd%25%25in07@cluster0-ohfo7.mongodb.net/test?retryWrites=true&w=majority`,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-    );
-    console.log('Connected to mongoDB cluster!');
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
-};
-
-initMongoDB().catch((err) => console.error(err));
+initMongoDB();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.render('signup');
-});
-
-app.use('/user', user); 
+app.use('/', main);
+app.use('/user', user);
 app.use('/buy', buy);
-app.use('/transactions', transactions)
-
-app.get('/log-in', (req, res) => {
-  res.render('login');
-});
-
-app.get('/sign-up', (req, res) => {
-  res.render('signup');
-});
-
-app.get('/portfolio', (req, res) => {
-  res.render('portfolio')
-})
+app.use('/transactions', transactions);
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}...`);
