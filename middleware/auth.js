9e8f54701e94e2ef3bb131cf9ae063386c530193
Enmanuel dlN <edelanuezlang@gmail.com>
2020-03-13 14:59:49 -0400

    add consistent return, switch to function expression

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,17 @@
 const jsonWebToken = require('jsonwebtoken');
 
-function auth(req, res, next) {
+const auth = (req, res, next) => {
   const token = req.header('token');
-
-  if (!token) return res.status(401).json({ message: 'Auth Error' });
-
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
   try {
     const decoded = jsonWebToken.verify(token, 'secret');
     req.user = decoded.user;
-    next();
+    return next();
   } catch (err) {
-    console.error(err);
-    res.status(500).send({ message: 'Invalid token' });
+    return res.status(500).send({ message: 'Invalid token' });
   }
-}
+};
 
 module.exports = auth;
